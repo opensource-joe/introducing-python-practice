c9340524b76dd3055445acc728f461979884d1c8
opensource-joe <joe.castle@sas.com>
2022-08-10 10:07:06 -0400

    working through ch7 exercises

--- ch7_exercises.py
+++ ch7_exercises.py
@@ -2,19 +2,37 @@
 
 # 7.1 Create a list called years_list, starting with the year of your birth, and each year thereafter until the year of your fifth birthday. For example, if you were born in 1980, the list would be years_list = [1980, 1981, 1982, 1983, 1984, 1985]. If you’re less than five years old and reading this book, I don’t know what to tell you.
 
+# years_list = [1975, 1976, 1977, 1978, 1979]
+
 # 7.2 In which year in years_list was your third birthday? Remember, you were 0 years of age for your first year.
+
+# print(years_list[3])
 
 # 7.3 In which year in years_list were you the oldest?
 
+# print(years_list[4])
+
 # 7.4 Make a list called things with these three strings as elements: "mozzarella", "cinderella", "salmonella".
+
+# things = ["mozzarella", "cindarella", "salmonella"]
 
 # 7.5 Capitalize the element in things that refers to a person and then print the list. Did it change the element in the list?
 
+# things[1] = 'Cindarella'
+
 # 7.6 Make the cheesy element of things all uppercase and then print the list.
+
+# things[0] = 'Mozzarella'
 
 # 7.7 Delete the disease element from things, collect your Nobel Prize, and print the list.
 
+# del things[2]
+# things.append('Noble Prize')
+# print(things)
+
 # 7.8 Create a list called surprise with the elements "Groucho", "Chico", and "Harpo".
+
+surprise = ['Groucho', 'Chico', 'Harpo']
 
 # 7.9 Lowercase the last element of the surprise list, reverse it, and then capitalize it.
 
