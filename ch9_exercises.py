4b4109f59aecaefc36527576941b46804919977f
opensource-joe <joe.castle@sas.com>
2022-08-20 00:26:22 -0400

    updated exercises for ch9

--- ch9_exercises.py
+++ ch9_exercises.py
@@ -75,6 +75,42 @@
 
 # 9.2 Define a generator function called get_odds() that returns the odd numbers from range(10). Use a for loop to find and print the third value returned.
 
+# def get_odds():
+#     '''return odd number from range(10)'''
+#     for number in range(1, 10, 2):
+#         yield number
+
+# count = 1
+# for number in get_odds():
+#     if count == 3:
+#         print('The third odd number is', number)
+#         break
+#     count +=1
+
 # 9.3 Define a decorator called test that prints 'start' when a function is called, and 'end' when it finishes.
 
+# def test(func):
+#     def new_func(*args, **kwargs):
+#         print('start')
+#         result = func(*args, **kwargs)
+#         print('end')
+#         return result
+#     return new_func
+
+# @test
+# def greeting():
+#     print('Greetings, Earthling')
+
+# greeting()
+
 # 9.4 Define an exception called OopsException. Raise this exception to see what happens. Then, write the code to catch this exception and print 'Caught an oops'.
+
+# class OopsException(Exception):
+#     pass
+
+# raise OopsException()
+
+# try:
+#     raise OopsException
+# except OopsException:
+#     print('Caught an oops')
